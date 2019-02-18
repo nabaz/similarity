974875e0c95aa3c2bbb050d329daa8024511efe6
Nabaz Maaruf <nabaz@santechno.net>
2019-02-18 17:11:06 -0500

    Update func.py
    cleaned up the code

--- func.py
+++ func.py
@@ -1,16 +1,10 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import csv
-file = './synopsis.csv'
-synopese =['The minute-by-minute story of the first attempt to bring down the World Trade Center, with eyewitness accounts from the people who were there. At 12:18 p.m. on February 26, 1993, an explosion rocked the giant World Trade Center in New York City, and America discovered how vulnerable it was to terrorism.',
- 'An explosion rocked the World Trade Center in February 1993 and witnesses describe their reactions to the terrorist attack.']
-
-
 import nltk, string
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download('punkt') # if necessary...
 
-
+syn_file = './synopsis.csv'
 stemmer = nltk.stem.porter.PorterStemmer()
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 
@@ -27,7 +21,7 @@ def cosine_sim(text1, text2):
     tfidf = vectorizer.fit_transform([text1, text2])
     return ((tfidf * tfidf.T).A)[0,1]
 try:
-    with open(file, encoding='utf-8') as csv_file:
+    with open(syn_file, encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         ppl = []
